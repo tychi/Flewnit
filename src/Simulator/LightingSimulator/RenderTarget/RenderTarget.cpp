1678c4536ff66a1a52acb5214c41b5d857ef36b2
tychi <markusschlueter@uni-koblenz.de>
2011-01-26 15:29:51 +0100

    backup commit; still implementing the RenderTarget class

--- src/Simulator/LightingSimulator/RenderTarget/RenderTarget.cpp
+++ src/Simulator/LightingSimulator/RenderTarget/RenderTarget.cpp
@@ -7,18 +7,135 @@
 
 #include "RenderTarget.h"
 
+#include "Common/CL_GL_Common.h"
+
 namespace Flewnit
 {
 
-RenderTarget::RenderTarget()
+//multiSample flag needed for render buffer generation
+RenderTarget::RenderTarget(String name, const Vector2Di& resolution, bool useStencilBuffer, int numMultisamples)
+: mName(name), mFrameBufferResolution(resolution), mUseStencilBuffer(useStencilBuffer), mNumMultisamples(numMultisamples)
 {
-	// TODO Auto-generated constructor stub
+	//default initialization
+	mDepthTestEnabled = true;
+	bool mStencilTestEnabled = mUseStencilBuffer;
+
+	for(int i= 0 ; i < __NUM_TOTAL_SEMANTICS__; i++)
+	{
+		mOwnedTexturePool[i] = 0;
+	}
+
+
+	mNumCurrentDrawBuffers = 0;
+	for(int i= 0 ; i < FLEWNIT_MAX_COLOR_ATTACHMENTS; i++)
+	{
+		mCurrentlyAttachedTextures[i] = INVALID_SEMANTICS;
+		//default stuff
+		mCurrentDrawBuffers[i] = GL_COLOR_ATTACHMENT0 + i;
+	}
+
+
+	glGenFramebuffers(1,&mFBO);
+
+	bind();
+
+	glGenRenderbuffers(1, &mGLRenderBufferHandle);
+	glBindRenderbuffer(GL_RENDERBUFFER, mGLRenderBufferHandle);
+
+	GLuint mFBO;
+	Vector2Di mFrameBufferResolution;
+
+	//i had no time to wrap the render buffer to my buffer concept; hope I won't need it
+	//somewhere else but here; Otherwise, wrapping will follow
+
+	GLuint mGLRenderBufferHandle;
+	//usually GL_DEPTH_COMPONENT32F with attachment type GL_DEPTH_ATTACHMENT
+	//or GL_DEPTH32F_STENCIL8 with attachment type GL_DEPTH_STENCIL_ATTACHMENT ;
+	GLenum mGLRenderBufferType;
+
+
 
 }
-
 RenderTarget::~RenderTarget()
 {
-	// TODO Auto-generated destructor stub
+	glDeleteRenderbuffers(1, &mGLRenderBufferHandle);
+	glDeleteFramebuffers(1, &mFBO);
+}
+
+
+void RenderTarget::bind(bool forReading)
+{
+	glBindFramebuffer(forReading ? GL_READ_FRAMEBUFFER : GL_FRAMEBUFFER, mFBO);
+}
+
+//calls bind() automatically
+void renderToAttachedTextures();
+
+void clear();
+
+void detachAllTextures();
+
+void RenderTarget::renderToScreen()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+void setEnableDepthBuffering(bool value);
+void setEnableStencilTest(bool value);
+
+void addTexture(Texture* tex);
+//can return NULL
+Texture* getTexture(BufferSemantics bs);
+void removeTexture(Texture* tex);
+
+void attachColorTexture(Texture* tex, int where);
+void attachStoredColorTexture(BufferSemantics which, int where) throw(BufferException);
+
+/**
+ * A texture will be created with the specified semantics if it doesn't exist;
+ * If it exists, it nothing will be done but printed a warning
+ */
+void requestCreateAndStoreTexture(BufferSemantics which);
+
+
+void attachDepthTexture(Texture2DDepth* depthTex)throw(BufferException);
+void renderDepthOnly();
+void detachDepthTexture();
+
+void RenderTarget::checkFrameBufferErrors()throw(BufferException)
+{
+    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
+    String statusString="";
+
+    switch (status) {
+    case GL_FRAMEBUFFER_COMPLETE:
+    	statusString="Framebuffer complete.";
+
+    case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+    	statusString= "Framebuffer incomplete: Attachment is NOT complete.";
+
+    case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+    	statusString= "Framebuffer incomplete: No image is attached to FBO.";
+
+    case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+    	statusString= "Framebuffer incomplete: Draw buffer.";
+
+    case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+    	statusString= "Framebuffer incomplete: Read buffer.";
+
+    case GL_FRAMEBUFFER_UNSUPPORTED:
+    	statusString= "Unsupported by FBO implementation.";
+
+    default:
+    	statusString= "glCheckFramebufferStatus Unknown error.";
+    }
+
+    if(status != GL_FRAMEBUFFER_COMPLETE)
+    {
+    	 LOG<<ERROR_LOG_LEVEL<<statusString;
+    	 throw(BufferException(statusString));
+    }
 }
 
 }
--- src/Simulator/LightingSimulator/RenderTarget/RenderTarget.h
+++ src/Simulator/LightingSimulator/RenderTarget/RenderTarget.h
@@ -3,10 +3,22 @@
  *
  *  Created on: Jan 26, 2011
  *      Author: tychi
+ *
+ * Class wrapping an OpenGL Framebuffer Object with optional maintainance capabilities for textures;
+ *
+ * You can request the creation of textures with desired semantics, and the internal format
+ * (data types, number of channels, precision, normalization etc.) will be handled for you.
+ * The precision is mostly 32bit per channel, but can be less, e.g. for normal map semantics;
+ * Will be subject to experimentation.
  */
 
 #pragma once
+
+
+
 #include "Common/BasicObject.h"
+
+#define FLEWNIT_MAX_COLOR_ATTACHMENTS 8
 
 namespace Flewnit
 {
@@ -17,13 +29,22 @@ class RenderTarget
 	FLEWNIT_BASIC_OBJECT_DECLARATIONS;
 public:
 	//multiSample flag needed for render buffer generation
-	RenderTarget(String name, bool useStencilBuffer = false, bool useMultisample = false);
+	RenderTarget(String name, const Vector2Di& resolution, bool useStencilBuffer = false, int numMultisamples = 0);
 	virtual ~RenderTarget();
 
-	void bind();
+
+	void bind(bool forReading = false);
+	//calls bind() automatically
+
+	void renderToAttachedTextures();
+
+	void clear();
+
+	void detachAllTextures();
 	void renderToScreen();
 
 	void setEnableDepthBuffering(bool value);
+	void setEnableStencilTest(bool value);
 
 	void addTexture(Texture* tex);
 	//can return NULL
@@ -33,10 +54,32 @@ public:
 	void attachColorTexture(Texture* tex, int where);
 	void attachStoredColorTexture(BufferSemantics which, int where) throw(BufferException);
 
-	tocontinue
+	/*
+	 * Getter for shaders which have to glBindFragDataLocation();
+	 * Throws exception when no texture with specified semantics is currently attached;
+	 */
+	int getAttachmentPoint(BufferSemantics which)const throw(BufferException);
 
-	void attachDepthTexture(Texture2DDepth* depthTex);
+	/**
+	 * A texture will be created with the specified semantics if it doesn't exist;
+	 * If it exists, it nothing will be done but printed a warning
+	 */
+	void requestCreateAndStoreTexture(BufferSemantics which);
+
+
+	///\{ Stuff useful for shadowmap generation
+	void attachDepthTexture(Texture2DDepth* depthTex)throw(BufferException);
+	void renderDepthOnly();
+	void detachDepthTexture();
+	///\}
+
+	void checkFrameBufferErrors()throw(BufferException);
+
+	inline String getName()const{return mName;}
+
 private:
+
+	String mName;
 
 	//In case of deferred rendering we need several textures for writing and reading
 	//available in every simulation stage. Hence, a Rendertarget in this framework is
@@ -47,12 +90,32 @@ private:
 
 	//tracker, what texture is attached where
 	//Initialized everywhere to INVALID_SEMANTICS
-	BufferSemantics	mCurrentlyAttachedTextures[8];
+	BufferSemantics	mCurrentlyAttachedTextures[FLEWNIT_MAX_COLOR_ATTACHMENTS];
+	GLenum	mCurrentDrawBuffers[FLEWNIT_MAX_COLOR_ATTACHMENTS];
+	GLsizei mNumCurrentDrawBuffers;
+
+	GLuint mFBO;
+	Vector2Di mFrameBufferResolution;
 
 	//i had no time to wrap the render buffer to my buffer concept; hope I won't need it
 	//somewhere else but here; Otherwise, wrapping will follow
+
 	GLuint mGLRenderBufferHandle;
+	//usually GL_DEPTH_COMPONENT32F with attachment type GL_DEPTH_ATTACHMENT
+	//or GL_DEPTH32F_STENCIL8 with attachment type GL_DEPTH_STENCIL_ATTACHMENT ;
 	GLenum mGLRenderBufferType;
+
+	bool mUseStencilBuffer;
+	int  mNumMultisamples;
+
+	bool mDepthTestEnabled;
+	bool mStencilTestEnabled;
+
+
+
+	//won't work supposedly with current hardware/drivers
+//	GLuint mGLRenderBufferStencilHandle;
+//	GLenum mGLRenderBufferStencilType; //usually GL_STENCIL_INDEX16
 
 };
 
